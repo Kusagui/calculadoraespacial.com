e0ecd1ec6afd5782d9b414bdac9f435da27a2c00
Carlos Machado <mcdario0223@gmail.com>
2022-04-18 22:56:58 -0500

    new code refactoring

---
+++ Scripts/planets.js
@@ -0,0 +1,40 @@
+let planets = [
+    {
+        'name' : 'Mercury',
+        'gravity': 3.7
+    },
+    {
+        'name' : 'Venus',
+        'gravity': 8.87
+    },
+    {
+        'name' : 'Earth',
+        'gravity': 9.8
+    },
+    {
+        'name' : 'Mars',
+        'gravity': 3.7
+    },
+    {
+        'name' : 'Jupiter',
+        'gravity': 24.8
+    },
+    {
+        'name' : 'Saturn',
+        'gravity': 10.44
+    },
+    {
+        'name' : 'Uranus',
+        'gravity': 8.87
+    },
+    {
+        'name' : 'Neptune',
+        'gravity': 11.15
+    },
+    {
+        'name' : 'Pluto',
+        'gravity': 0.62
+    }
+];
+
+export { planets }
