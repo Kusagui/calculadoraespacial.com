9792b0d4598a0a0ccdb05736cc98b0ab5b83d909
Carlos Machado <mcdario0223@gmail.com>
2022-04-18 22:55:31 -0500

    Code refactoring modularization

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,90 +1,178 @@
-function getAnswers() {
-    
+const planets = [
+    {
+        'name' : 'Mercury',
+        'gravity': 3.7
+    },
+    {
+        'name' : 'Venus',
+        'gravity': 8.87
+    },
+    {
+        'name' : 'Earth',
+        'gravity': 9.8
+    },
+    {
+        'name' : 'Mars',
+        'gravity': 3.7
+    },
+    {
+        'name' : 'Jupiter',
+        'gravity': 24.8
+    },
+    {
+        'name' : 'Saturn',
+        'gravity': 10.44
+    },
+    {
+        'name' : 'Uranus',
+        'gravity': 8.87
+    },
+    {
+        'name' : 'Neptune',
+        'gravity': 11.15
+    },
+    {
+        'name' : 'Pluto',
+        'gravity': 0.62
+    }
+];
+
+// Obteniendo el los planetas deseados por el usuario
+function getPlanetsInput() {
     // Tomando checkboxes dentro de un nodeList
     let selectedPlanets = document.querySelectorAll('input[type=checkbox]');  
-
+    
     // Convirtiendo nodeList a un array
     let InitialAnswers = [...selectedPlanets];
-
+    
     // Variable que contenga los planetas seleccionados
     let realAnswers = [];
-
+    
     // Empujando cada planeta seleccionado al contenedor de respuestas
     InitialAnswers.forEach((answer) => {
-
+    
         if (answer.checked) {
             realAnswers.push(answer);
         }
-
+    
     });
 
-    // Tomando valor de input de usuario
-    let userWeight = document.querySelector('#earth').value;
-    const EARTHGRAVITY = 9.8;
+    return realAnswers;
+}
 
-    // Función para calcular mi peso en otro planeta
-    function calculateMyWeight(userW, planetG) {
-        return (userW * planetG) / EARTHGRAVITY;
-    }
+// Get user input weight
+function getUserWeight() {
+    let userWeight = document.querySelector('#earth');
+    let weightValue = parseInt(userWeight.value);
+    
+    return weightValue;
+}
 
-    // Valores de cada planeta disponible
-    let possiblePlanets = [
-        {
-            'name' : 'Mercury',
-            'gravity': 3.7
-        },
-        {
-            'name' : 'Venus',
-            'gravity': 8.87
-        },
-        {
-            'name' : 'Mars',
-            'gravity': 3.7
-        },
-        {
-            'name' : 'Jupiter',
-            'gravity': 24.8
-        },
-        {
-            'name' : 'Saturn',
-            'gravity': 10.44
-        },
-        {
-            'name' : 'Uranus',
-            'gravity': 8.87
-        },
-        {
-            'name' : 'Neptune',
-            'gravity': 11.15
-        },
-        {
-            'name' : 'Pluto',
-            'gravity': 0.62
-        }
-    ];
+// Calculate weight in other planets
+function calculateMyWeight(userW, planetG) {
+    const earthGravity = planets[2].gravity;
+    let myNewWeight = (userW * planetG) / earthGravity;
+    return myNewWeight;
+}
 
-    // Agregando gravedad como valor y propiedad a cada input comparado con cada planeta disponible
-    for (let i = 0; i < realAnswers.length; i++) {
-        let planet = realAnswers[i];
+// Creating the answers by planet card
+function answerListItem(planetName, planetGravity, finalUW) {
+    
+    let pName = planetName;
+    let pGravity = planetGravity;
+    let uWeight = finalUW; 
+    
+    const answerContainer = document.createElement('li');
+    answerContainer.classList.add('answer-container');
 
-        for (let p = 0; p < possiblePlanets.length; p++ ) {
-            if (planet.value === possiblePlanets[p].name) {
-                planet.dataset.planetGravity = possiblePlanets[p].gravity;
+    const answerP = document.createElement('div');
+    answerP.classList.add('answer-planet');
+
+    const imageContainer = document.createElement('div');
+    imageContainer.classList.add('image-container');
+
+    const planetImage = document.createElement('img');
+    planetImage.classList.add('planet-image');
+
+    imageContainer.appendChild(planetImage);
+
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('text-container');
+
+    const answerTitle = document.createElement('h3');
+    answerTitle.classList.add('answer-title');
+    answerTitle.textContent = `The gravity of the planet ${pName} is: ${pGravity}`;
+
+    const answerText = document.createElement('p');
+    answerText.classList.add('answer-text');
+    answerText.textContent = `${uWeight}`;
+
+    textContainer.append(answerTitle, answerText);
+
+    answerP.append(imageContainer, textContainer);
+
+    answerContainer.appendChild(answerP);
+
+    return answerContainer;
+}
+
+// calculating and  pushing each answer to a container
+function gettingPlanetAnswers(cPlanets) {
+    let arrayTotalAnswers = [];
+    let nodeTotalAnswers = document.createElement('ul');
+    nodeTotalAnswers.classList.add('answers-list');
+
+    let finalTitle = document.createElement('h3');
+    finalTitle.classList.add('final-planets-title');
+    finalTitle.textContent = 'Your weight in the chosen planets';
+
+    // Getting user weight
+    let userWeight = getUserWeight();
+
+    for (let i = 0; i < cPlanets.length; i++) {
+        let planet = cPlanets[i];
+
+        for (let p = 0; p < planets.length; p++ ) {
+            let chosenPlanet = planets[p].name;
+            let chosenGravity = planets[p].gravity;
+
+            if (planet.value === chosenPlanet) {                
+        
+                let finalUserWeight = calculateMyWeight(userWeight, chosenGravity);
+
+                let answerPlanet = answerListItem(chosenPlanet, chosenGravity, finalUserWeight);
+
+                arrayTotalAnswers.push(answerPlanet);
             }
         }        
-                
-        // Haciendo el calculo de peso por cada planeta
-        let finalWeight = calculateMyWeight(userWeight, planet.dataset.planetGravity);
-        console.log('Tu peso en ' + planet.value + ' es de: ' + finalWeight.toFixed(2));
-    }   
+    }
 
-    // Creando contenedor de respuestas
-    const answerContainer = document.createElement('div');
+    nodeTotalAnswers.append(finalTitle, [...arrayTotalAnswers]);
+    return nodeTotalAnswers;
+}
+
+
+
+// Main function
+function startProgram() {
+    // Getting Planets
+    let chosenPlanets = getPlanetsInput();
+
+    // Getting user weight
+    let userWeight = getUserWeight();
+    
+    // Calculating my weight
+    let finalAnswers = gettingPlanetAnswers(chosenPlanets);
+    console.log(finalAnswers);
+    
+    // Creando contenedor de sección de respuestas
+    const answerContainer = document.createElement('section');
     answerContainer.classList.add('answer-container');
 
     // The close tab of main container 
     const closeIcon = document.createElement('img');
     closeIcon.classList.add('close-icon');
+    closeIcon.src = '../assets/icons/close.png';
 
     const iconContainer = document.createElement('div');
     iconContainer.classList.add('icon-container');
@@ -94,211 +182,27 @@ function getAnswers() {
     // Title 
     const answerTitle = document.createElement('h2');
     answerTitle.classList.add('answer-title');
-    answerTitle.textContent = 'Your weight in the chosen planets';
+    answerTitle.textContent = 'Hi Friend!';
 
-    // Earth Card
-    
-}
-
-
-const button = document.querySelector('.button');
-button.addEventListener('click', getAnswers);
+    const inputRemainder = document.createElement('p');
+    inputRemainder.classList.add('user-input-reminder');
+    inputRemainder.textContent = `Your weight in the planet ${planets[2].name} is ${userWeight}`;
 
 
-
-
-
-
-/* 
-function calculateMyWeight() {
-
-    let userWeight = document.querySelector('#earth').value;
-    
-    let mercury = document.querySelector('#mercury');
-    let venus = document.querySelector('#venus');
-    let mars = document.querySelector('#mars');
-    let jupiter = document.querySelector('#jupiter');
-    let saturn = document.querySelector('#saturn');
-    let uranus = document.querySelector('#uranus');
-    let neptune = document.querySelector('#neptune');
-    let pluto = document.querySelector('#pluto');
-    
-    const EARTHGRAVITY = 9.8;
-    
-    let chosenPlanets = [];
-    
-    if (mercury.checked) {
-        const gMercury = 3.7;
-        
-        chosenPlanets.push({
-            'name': mercury.value,
-            'gravity': gMercury
-        });
-    }
-    
-    if (venus.checked) {
-        const gVenus = 8.87;
-        
-        chosenPlanets.push({
-            'name': venus.value,
-            'gravity': gVenus
-        });
-    }
-    
-    if (mars.checked) {
-        const gMars = 3.7;
-        
-        chosenPlanets.push({
-            'name': mars.value,
-            'gravity': gMars
-        });
-    }
-    
-    if (jupiter.checked) {
-        const gJupiter = 24.8;
-        
-        chosenPlanets.push({
-            'name': jupiter.value,
-            'gravity': gJupiter
-        });
-    }
-    
-    if (saturn.checked) {
-        const gSaturn = 10.44;
-        
-        chosenPlanets.push({
-            'name': saturn.value,
-            'gravity': gSaturn
-        });
-    }
-    
-    if (uranus.checked) {
-        const gUranus = 8.87;
-        
-        chosenPlanets.push({
-            'name': uranus.value,
-            'gravity': gUranus
-        });
-    }
-    
-    if (neptune.checked) {
-        const gNeptune = 11.15;
-        
-        chosenPlanets.push({
-            'name': neptune.value,
-            'gravity': gNeptune
-        });
-    }
-    
-    if (pluto.checked) {
-        const gPluto = 0.62;
-        
-        chosenPlanets.push({
-            'name': pluto.value,
-            'gravity': gPluto
-        });
-    }
-
-    function calculateMyWeight(n1, n2) {
-        return (n1 * n2) / EARTHGRAVITY;
-    }
-
-    // Main container 
-    const answerContainer = document.createElement('div');
-    answerContainer.classList.add('answer-container');   
-    
-    // The close tab of main container 
-    const closeIcon = document.createElement('img');
-    closeIcon.classList.add('close-icon');
-    closeIcon.src = "../assets/icons/close.png"
-    closeIcon.width = "24"
-
-    const iconContainer = document.createElement('div');
-    iconContainer.classList.add('icon-container');
-
-    iconContainer.append(closeIcon);
-
-    // Title 
-    const answerTitle = document.createElement('h2');
-    answerTitle.classList.add('answer-title');
-    answerTitle.textContent = 'Your weight in the chosen planets';
-
-    // Earth Card 
-    const earthCard = document.createElement('article');
-    earthCard.classList.add('earth-card');
-
-    const earthImage = document.createElement('img');
-    earthImage.src = '';
-    earthImage.classList.add('earth--image');
-
-    const earthImageContainer = document.createElement('figure');
-    earthImageContainer.classList.add('image-container');
-    earthImageContainer.append(earthImage);
-
-    const earthCardTitle = document.createElement('h3');
-    earthCardTitle.classList.add('earth-card--title');
-    earthCardTitle.textContent = `The gravity in Earth is: ${EARTHGRAVITY}m/s2`
-
-    const earthCardText = document.createElement('p');
-    earthCardText.classList.add('earth-card--text');
-    earthCardText.textContent = `Then Your weight is: ${userWeight}Kg`;
-
-    const earthTextContainer = document.createElement('div');
-    earthTextContainer.classList.add('earth-text-container');
-    earthTextContainer.append(earthCardTitle, earthCardText);
-
-    earthCard.append(earthImageContainer, earthTextContainer);
-
-    const planetCards = [];
-
-    for (let i = 0; i < chosenPlanets.length; i++) {
-
-        let planetGravity = chosenPlanets[i].gravity;
-        let finalWeight = calculateMyWeight(userWeight, planetGravity);
-
-        // Planet card 
-
-        const planetCard = document.createElement('article');
-        planetCard.classList.add('planet-card');
-
-        const planetImage = document.createElement('img');
-        planetImage.src = '';
-        planetImage.classList.add('planet-card--image');
-
-        const imageContainer = document.createElement('figure');
-        imageContainer.classList.add('planet-image-container');
-        imageContainer.append(planetImage);
-
-        const planetCardTitle = document.createElement('h3');
-        planetCardTitle.classList.add('planet-card--title');
-        planetCardTitle.textContent = `The gravity in ${chosenPlanets[i].name} is: ${chosenPlanets[i].gravity}m/s2`
-
-        const planetCardText = document.createElement('p');
-        planetCardText.classList.add('earth-card--text');
-        planetCardText.textContent = `Then Your weight is: ${finalWeight.toFixed(2)}kg`;
-
-        const resultContainer = document.createElement('div');
-        resultContainer.classList.add('result-container');
-        resultContainer.append(planetCardTitle, planetCardText)
-
-        planetCard.append(imageContainer, resultContainer);
-
-        planetCards.push(planetCard);
-
-        console.log(`Your weight in ${chosenPlanets[i].name} is: ${finalWeight.toFixed(2)}kg`);
-    }  
-
-    answerContainer.append(iconContainer, answerTitle, earthCard);
-    answerContainer.append(...planetCards);
+    answerContainer.append(iconContainer, answerTitle, inputRemainder, finalAnswers);
 
     const body = document.querySelector('.main-body');
-    body.appendChild(answerContainer);
+    body.appendChild(answerContainer);  
 
     const closeNode = () => {
         body.removeChild(answerContainer);
     }
-
-    closeIcon.addEventListener('click', closeNode);
+    
+    closeIcon.addEventListener('click', closeNode);  
 }
 
- */
+
+
+const button = document.querySelector('.button');
+button.addEventListener('click', startProgram);
+
