036bd450d8f0660d35be49ccedd2b436a3530b53
Carlos Machado <mcdario0223@gmail.com>
2022-05-04 18:06:49 -0500

    separación de archivo de planetas

--- app.js
+++ app.js
@@ -1,41 +1,4 @@
-const planets = [
-    {
-        'name' : 'Mercury',
-        'gravity': 3.7
-    },
-    {
-        'name' : 'Venus',
-        'gravity': 8.87
-    },
-    {
-        'name' : 'Earth',
-        'gravity': 9.8
-    },
-    {
-        'name' : 'Mars',
-        'gravity': 3.7
-    },
-    {
-        'name' : 'Jupiter',
-        'gravity': 24.8
-    },
-    {
-        'name' : 'Saturn',
-        'gravity': 10.44
-    },
-    {
-        'name' : 'Uranus',
-        'gravity': 8.87
-    },
-    {
-        'name' : 'Neptune',
-        'gravity': 11.15
-    },
-    {
-        'name' : 'Pluto',
-        'gravity': 0.62
-    }
-];
+import { planets } from "./source/planets.js";
 
 // Obteniendo el los planetas deseados por el usuario
 function getPlanetsInput() {
